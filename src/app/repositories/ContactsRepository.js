8f362fc739e25d88d76c4d6299d1878300f3b2ee
Jotta11 <93175796+Jotta11@users.noreply.github.com>
2022-12-09 03:13:27 -0300

    Updates

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -21,11 +21,33 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
   }
+
   findById(id) {
     return new Promise((resolve) => resolve(
       contacts.find((contact) => contact.id === id),
     ));
   }
+
+  findByEmail(email) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.email === email),
+    ));
+  }
+
+  create(name, email, phone, category_id) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts.push(newContact)
+      resolve(newContact);
+    })
+  }
+
   delete(id) {
     return new Promise((resolve) => {
       contacts = contacts.filter((contact) => contact.id !== id);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
 app.use(routes);
 
 app.listen(3001, () => console.log('🚀 Server started at http://localhost:3001'));
